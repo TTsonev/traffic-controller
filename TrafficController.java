9a2a860d4d4197dfe054362a9edb312db1a9d352
TTsonev <80343194+TTsonev@users.noreply.github.com>
2023-02-11 20:28:34 +0200

    Update TrafficController.java

--- TrafficController.java
+++ TrafficController.java
@@ -1,6 +1,6 @@
 public interface TrafficController {
 	void enterRight(Vehicle v); // only returns if bridge is empty; blocks otherwise
-    void enterLeft(Vehicle v);  // only returns if bridge is empty; blocks otherwise
-    void leaveLeft(Vehicle v);  // must be called after enterRight
-    void leaveRight(Vehicle v); // must be called after enterLeft
+	void enterLeft(Vehicle v);  // only returns if bridge is empty; blocks otherwise
+	void leaveLeft(Vehicle v);  // must be called after enterRight
+	void leaveRight(Vehicle v); // must be called after enterLeft
 }
